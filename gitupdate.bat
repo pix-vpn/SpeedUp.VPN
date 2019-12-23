c60f0cddf406dafaaf199d0ace9575c0d4c51bfe
bannedbook <banned.ebook@gmail.com>
2019-12-23 18:25:22 +1100

    fix rawgit block

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,6 +1,8 @@
 git config --global core.autocrlf true
 git pull origin master
 git add -A
-git commit -m "update"
+git commit -m "fix rawgit block"
 git push origin master
+git tag -a v1.0.1 -m "release v1.0.1"
+git push origin --tags
 pause
